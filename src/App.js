2f09c90b15c75a192578a7c964c5f464e9def880
Yanti Suryati <yantisc@gmail.com>
2019-07-09 19:29:25 +0100

    refactored the App code and working on layout and style

--- src/App.js
+++ src/App.js
@@ -1,42 +1,92 @@
 import React, {Component} from 'react';
-import Form from './Form';
 import './App.css';
 
 class App extends Component {
   state = {
-    inputs:{}
-  };
+    firstName: '',
+    lastName: '',
+    hobby:'',
+    summary:''
+}
 
-  // onChange = updatedValue => {
-  //   this.setState({
-  //     data: {
-  //       ...this.state.data,
-  //       ...updatedValue
-  //     }
-  //   })
-  // }
+  onSubmit = (e) => {
+    e.preventDefault();
+    const firstN = this.state.firstName;
+    const lastN = this.state.lastName;
+    const myHobby = this.state.hobby;
+    this.setState({
+        firstName:'',
+        lastName:'',
+        hobby:'',
+        summary:
+        ( 'My full name is ' + firstN + ' ' + lastN + ' ' + 'and one of my hobbies is' + ' ' + myHobby )    
+    });
+};
 
-  onSubmit = inputs => {
-    this.setState({inputs})
-    console.log({inputs});
-  };
-
-  
   render () {
     return (
+      
       <div className="App">
-        <h3 className="header">Your Personal Details</h3>
-        <Form onSubmit={inputs=> this.onSubmit(inputs)}/>
-        <p>
-          {JSON.stringify(this.state.inputs,null,2)}
-        </p>
+        <div className="header">
+          Sainsbury's
+          <p>Technical Test By Yanti Suryati</p>
+        </div>
+
+        <h3 className="title">Your Personal Details</h3>
+
+        <form className="form">
+            <label className="label">
+            First Name: 
+                <input 
+                name= 'firstName'
+                type='text'
+                placeholder='First Name' 
+                value={this.state.firstName} 
+                onChange={e => this.setState({firstName: e.target.value})}
+                />
+            </label>
+           
+            <label className="label">
+            Last Name:
+                <input 
+                name='lastName'
+                type='text'
+                placeholder='Last Name' 
+                value={this.state.lastName} 
+                onChange={e => this.setState({lastName: e.target.value})}
+                />
+            </label>
+
+            <label className="label">
+            Hobby:
+                <select className="select"
+                name='hobby'
+                value={this.state.hobby}
+                onChange = { e => this.setState({hobby: e.target.value})}
+                >
+                    <option value=''>Select from the list</option>
+                    <option value='Travelling'>Travelling</option>
+                    <option value='Cooking'>Cooking</option>
+                    <option value='Coding'>Coding</option>
+                </select>
+            </label>
+           
+          
+            <button className="button" type='submit' onClick={e => this.onSubmit(e)}>Submit</button>
         
+            <textarea 
+                name='summary'
+                placeholder="Your details will be displayed here..." 
+                value={this.state.summary}
+                onChange = { e => this.setState()}
+                >
+            </textarea>
+        </form>
+
       </div>
     );
   }
 }
-
-
 
 export default App;
 
