d8d7aa6e204dbed95d1eec6c3aee460d852999aa
Yanti Suryati <yantisc@gmail.com>
2019-07-15 15:26:51 +0100

    yarn updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Header from './components/Header';
-import { useMediaQuery } from 'react-responsive';
 import './App.css';
 
 class App extends Component {
